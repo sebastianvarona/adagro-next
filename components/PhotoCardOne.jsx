b41f768b443c9548994adc207d13d62da94d5e04
Sebastian Varona <sebastianvarona@gmail.com>
2022-05-24 00:50:58 -0500

    header and qs section made

---
+++ components/PhotoCardOne.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PhotoCardOne(props) {
+  return (
+    <div
+      className="w-full h-full rounded-2xl bg-cover bg-center drop-shadow-md"
+      {...props}
+    ></div>
+  );
+}
